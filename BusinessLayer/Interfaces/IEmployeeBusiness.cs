6c5f956a5e36f533ee9496d2fdb81fd33c5bb100
chaithanya <varma@gmail.com>
2024-02-13 20:36:16 +0530

    business layer

---
+++ BusinessLayer/Interfaces/IEmployeeBusiness.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Interfaces
+{
+    public interface IEmployeeBusiness
+    {
+        public object GetUserDetails();
+
+    }
+}
---
+++ BusinessLayer/Services/EmployeeBusiness.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessLayer.Interfaces;
+using RepositoryLayer.Interfaces;
+
+namespace BusinessLayer.Services
+{
+    public class EmployeeBusiness : IEmployeeBusiness
+    {
+        private readonly IEmployeeRepository _repository;
+
+        public EmployeeBusiness(IEmployeeRepository repository)
+        {
+            _repository = repository;
+        }
+        public object GetUserDetails()
+        {
+            return _repository.GetUserDetails();
+        }
+
+    }
+}
